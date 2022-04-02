4d3c4617de78889d5213e95fb694debfbd7ff740
Zafar1704 <zafarik79@gmail.com>
2022-04-02 15:51:40 -0400

    updated test case 3.1

--- src/test_cases/TC1003.java
+++ src/test_cases/TC1003.java
@@ -1,11 +1,13 @@
 package test_cases;
 
-public class TC003 {
+public class TC1003 {
     public static void main(String[] args) {
 
         // first lines
 
         // second line
+
+        // third lane
 
     }
 }
